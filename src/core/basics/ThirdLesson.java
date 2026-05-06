bc52b01a39c4f5a578adfc2d79359b87cfbd3724
Artem <cartem33@gmail.com>
2026-05-06 21:02:35 +0200

    Conditional constructions

---
+++ src/core/basics/ThirdLesson.java
@@ -0,0 +1,14 @@
+package core.basics;
+
+public class ThirdLesson {
+    public static void main(String[] args) {
+        int a = 6;
+        if (a % 2 == 0) {
+            System.out.println("Число " + a + " чётное");
+        } else if (a % 2 != 0) {
+            System.out.println("Число " + a + " нечётное");
+        } else {
+            System.out.println("хз чо написать");
+        }
+    }
+}
