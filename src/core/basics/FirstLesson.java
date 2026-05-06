8badc0758c517067b8d9f30e615ea8f77f580ae8
Artem <cartem33@gmail.com>
2026-05-06 19:21:36 +0200

    Variablen und primitive Datentypen

---
+++ src/core/basics/FirstLesson.java
@@ -0,0 +1,14 @@
+package core.basics;
+
+public class FirstLesson {
+    public static void main(String[] args) {
+        byte age = 23;
+        float temperature = 36.6f;
+        boolean trust = true;
+        char firstSymbol = 'A';
+        System.out.println(age);
+        System.out.println(temperature);
+        System.out.println(trust);
+        System.out.println(firstSymbol);
+    }
+}
