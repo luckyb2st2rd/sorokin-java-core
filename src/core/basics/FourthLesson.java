81db1ca928d8fb353afd571fcf14453eda2726ef
Artem <cartem33@gmail.com>
2026-05-15 01:12:53 +0200

    Cycles

---
+++ src/core/basics/FourthLesson.java
@@ -0,0 +1,15 @@
+package core.basics;
+
+public class FourthLesson {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+        int count = 10;
+        while (count >= 1) {
+            System.out.println(count);
+            count--;
+        }
+    }
+}
