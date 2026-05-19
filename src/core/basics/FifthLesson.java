73358b4f2c863593049bb6b96781fd7874ea3a4e
Artem <cartem33@gmail.com>
2026-05-19 20:49:48 +0200

    Arrays edit

--- src/core/basics/FifthLesson.java
+++ src/core/basics/FifthLesson.java
@@ -5,13 +5,20 @@ import java.util.Arrays;
 public class FifthLesson {
     public static void main(String[] args) {
         int[] numbers = new int[5];
-        
+
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2;
         }
-        System.out.println(Arrays.toString(numbers));
+        printArray(numbers);
 
         numbers[0] = 1;
-        System.out.println("Массив после замены элемента: " + Arrays.toString(numbers));
+        System.out.println("Массив после замены элемента");
+        printArray(numbers);
+    }
+
+    public static void printArray(int[] array) {
+        for (int num : array) {
+            System.out.println(num);
+        }
     }
 }
