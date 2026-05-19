313a8b11faf0e3591a53122f5bc2b510de906ed2
Artem <cartem33@gmail.com>
2026-05-19 20:05:50 +0200

    Arrays edit

--- src/core/basics/FifthLesson.java
+++ src/core/basics/FifthLesson.java
@@ -1,22 +1,17 @@
 package core.basics;
 
-import java.util.StringJoiner;
+import java.util.Arrays;
 
 public class FifthLesson {
     public static void main(String[] args) {
-        StringJoiner sj = new StringJoiner(" ", "[", "]");
         int[] numbers = new int[5];
+        
         for (int i = 0; i < numbers.length; i++) {
             numbers[i] = i * 2;
-            sj.add(String.valueOf(numbers[i]));
         }
-        System.out.println(sj);
+        System.out.println(Arrays.toString(numbers));
 
-        StringJoiner sj1 = new StringJoiner(" ", "[", "]");
         numbers[0] = 1;
-        for (int number : numbers) {
-            sj1.add(String.valueOf(number));
-        }
-        System.out.println("Массив после замены первого элемента: " + sj1);
+        System.out.println("Массив после замены элемента: " + Arrays.toString(numbers));
     }
 }
